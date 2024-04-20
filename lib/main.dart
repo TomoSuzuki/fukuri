79237f656e47c7cd756f4da4d0275e8c911afe0e
mbp2021early <provisionale2@mbp2021early.local>
2024-04-20 10:29:21 +0900

    compound interest calculation

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Fukuri'),
     );
   }
 }
@@ -31,10 +31,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  double _simpleinterest = 100;
+  double _result = 100;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      _simpleinterest = _simpleinterest + 5.0;
+      _result = _result * 1.05;
     });
   }
 
@@ -50,10 +54,24 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'nunber of count :',
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'simple interest 5% :',
+            ),
+            Text(
+              '$_simpleinterest',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'result of compound interest calculation:',
+            ),
+            Text(
+              '$_result',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
