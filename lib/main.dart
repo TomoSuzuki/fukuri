d44df7c06351004ee33215a22cdf9b96bc9340d2
mbp2021early <provisionale2@mbp2021early.local>
2024-04-27 13:47:48 +0900

    CupertinoButton

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   double _simpleinterest = 100;
   double _result = 100.0;
   String _disp = '100.0';
+  List<String> fruits = ['りんご', 'ぶどう', 'みかん'];
 
   void _incrementCounter() {
     setState(() {
@@ -50,6 +51,40 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _start = selectedItem;
     });
+  }
+
+  final List<String> _fruitNames = [
+    'Apple',
+    'Mango',
+    'Banana',
+    'Orange',
+    'Pineapple',
+    'Strawberry',
+  ];
+  final double _kItemExtent = 32.0;
+
+  int _selectedFruit = 0;
+
+  // This shows a CupertinoModalPopup with a reasonable fixed height which hosts CupertinoPicker.
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 216,
+        padding: const EdgeInsets.only(top: 6.0),
+        // The Bottom margin is provided to align the popup above the system navigation bar.
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        // Provide a background color for the popup.
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        // Use a SafeArea widget to avoid system overlaps.
+        child: SafeArea(
+          top: false,
+          child: child,
+        ),
+      ),
+    );
   }
 
   @override
@@ -90,6 +125,39 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '\$ $_disp',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            CupertinoButton(
+              padding: EdgeInsets.zero,
+              // Display a CupertinoPicker with list of fruits.
+              onPressed: () => _showDialog(
+                CupertinoPicker(
+                  magnification: 1.22,
+                  squeeze: 1.2,
+                  useMagnifier: true,
+                  itemExtent: _kItemExtent,
+                  // This sets the initial item.
+                  scrollController: FixedExtentScrollController(
+                    initialItem: _selectedFruit,
+                  ),
+                  // This is called when selected item is changed.
+                  onSelectedItemChanged: (int selectedItem) {
+                    setState(() {
+                      _selectedFruit = selectedItem;
+                    });
+                  },
+                  children:
+                      List<Widget>.generate(_fruitNames.length, (int index) {
+                    return Center(child: Text(_fruitNames[index]));
+                  }),
+                ),
+              ),
+              // This displays the selected fruit name.
+              child: Text(
+                _fruitNames[_selectedFruit],
+                style: const TextStyle(
+                  fontSize: 22.0,
+                ),
+              ),
             ),
             ElevatedButton(
               child: const Text('Pickerを表示！'),
